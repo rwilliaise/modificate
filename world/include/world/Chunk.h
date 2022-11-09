6e829cc33ca80fd57a6f29e25be86e7e6ee4c6ce
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2022-11-09 14:46:08 -0800

    World changes

--- world/include/world/Chunk.h
+++ world/include/world/Chunk.h
@@ -2,18 +2,27 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 #include <string>
-#include <map>
+
+#include <glm/ext/vector_uint3_sized.hpp>
+
+#include "World.h"
+
+constexpr auto CHUNK_SIZE = 16;
 
 namespace sh {
 
 	class Chunk {
 	public:
-
+		bool setBlock(glm::u8vec3 pos, std::string identifier);
 
 	private:
+		friend class World;
 
-		std::map<std::string, uint32_t> palette;
+		std::shared_ptr<World> world;
+		uint16_t blocks[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE]; // 8192 bytes
+
 	};
 
 } // sh
---
+++ world/include/world/World.h
@@ -0,0 +1,33 @@
+
+#pragma once 
+
+#include <unordered_map>
+#include <cstdint>
+#include <string>
+
+#include <glm/ext/vector_int3.hpp>
+#include <glm/gtx/hash.hpp>
+
+#include "Block.h"
+
+namespace sh {
+	class Chunk;
+
+	class World {
+	public:
+
+		bool setBlock(glm::ivec3 pos, std::string id);
+
+	private:
+		friend class Chunk;
+
+		uint16_t next = 0;
+		std::unordered_map<uint16_t, std::string> palette;
+		std::unordered_map<glm::i16vec3, Chunk> chunks;
+
+		std::unordered_map<std::string, Block> registered;
+		
+	};
+}
+
+#include "Chunk.h"
