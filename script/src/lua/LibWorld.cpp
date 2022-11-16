f2e793b98424211025cf5b48d8e3ea3f5bd79892
rwilliaise <rwilliaise@users.noreply.github.com>
2022-11-15 19:17:07 -0800

    Small world changes

--- script/src/lua/LibWorld.cpp
+++ script/src/lua/LibWorld.cpp
@@ -1,19 +1,24 @@
 
 #include "LibWorld.h"
+#include <world/World.h>
 
 namespace sh {
 
-	static int setBlock(lua_State *L) {
+	static World *getWorld(lua_State *L) {
 		lua_getfield(L, LUA_REGISTRYINDEX, LUA_WORLD);
-		luaL_checkudata(L, -1, LUA_WORLD);
+		return (World *) luaL_checkudata(L, -1, LUA_WORLD);
+	}
+
+	static int setBlock(lua_State *L) {
+		
 	}
 
 	const static luaL_Reg worldlib[] = {
-		{"setBlock", 
+		{"setBlock", setBlock},
 	};
 
 	int openlib_world(lua_State *L) {
-		luaL_newlib(L, l);
+		luaL_newlib(L, worldlib);
 		return 1;
 	}
 
