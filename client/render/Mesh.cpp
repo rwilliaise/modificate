d4cfc5594983b7b21397f9c889764e881f17bd3c
rwilliaise <rwilliaise@users.noreply.github.com>
2022-11-27 21:05:47 -0800

    Air block

--- client/render/Mesh.cpp
+++ client/render/Mesh.cpp
@@ -1,8 +1,9 @@
 
 #include "Mesh.h"
 
-#include <iostream>
 #include <stdexcept>
+#include <iostream>
+#include <vector>
 
 namespace r {
 
@@ -20,14 +21,20 @@ namespace r {
 		auto world = chunk.getWorld();
 		const auto &palette = world->getPalette();
 
+		std::vector<glm::vec3> data;
+
 		try {
 			for (size_t x = 0; x < CHUNK_SIZE; x++) {
 				for (size_t y = 0; y < CHUNK_SIZE; y++) {
 					for (size_t z = 0; z < CHUNK_SIZE; z++) {
 						uint16_t pBlock = chunk.getBlock(x, y, z);
+
+						if (pBlock == sh::AIR_BLOCK) {
+							continue;
+						}
+
 						auto id = palette.at(pBlock);
 						auto block = world->registered.at(id);
-						
 					}
 				}
 			}
--- shared/include/shared/World.h
+++ shared/include/shared/World.h
@@ -11,6 +11,8 @@
 #include <glm/gtx/hash.hpp>
 
 namespace sh {
+	const auto AIR_BLOCK = 0;
+
 	class Block;
 	class Chunk;
 
@@ -26,7 +28,7 @@ namespace sh {
 	private:
 		friend class Chunk;
 
-		uint16_t next = 0;
+		uint16_t next = 1;
 
 		std::unordered_map<uint16_t, std::string> palette;
 		std::unordered_map<glm::i16vec3, Chunk> chunks;
