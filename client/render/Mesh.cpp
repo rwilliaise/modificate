79121c22590855e8f920111781374868311682a9
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2022-11-28 16:15:31 -0800

    Mesh SIMD optimzations, TODO.md

--- client/render/Mesh.cpp
+++ client/render/Mesh.cpp
@@ -1,11 +1,21 @@
 
 #include "Mesh.h"
+#include "shared/Chunk.h"
 
+#include <glm/ext/vector_int4.hpp>
+#include <pmmintrin.h>
+#include <smmintrin.h>
 #include <stdexcept>
 #include <iostream>
 #include <vector>
+#include <xmmintrin.h>
 
 namespace r {
+
+	static const glm::ivec4 SIDES[] = {
+		glm::ivec4(1, 0, 0, 0), glm::ivec4(0, 1, 0, 0), glm::ivec4(0, 0, 1, 0), // west, up, north
+		glm::ivec4(-1, 0, 0, 0), glm::ivec4(0, -1, 0, 0), glm::ivec4(0, 0, -1, 0), // east, down, south 
+	};
 
 	Mesh::Mesh() {
 		glGenVertexArrays(1, &vao);
@@ -35,6 +45,54 @@ namespace r {
 
 						auto id = palette.at(pBlock);
 						auto block = world->registered.at(id);
+
+#ifdef R_SIMD_ENABLED
+						glm::ivec4 pos(x, y, z, 0);
+#else
+						glm::ivec3 pos(x, y, z);
+#endif
+
+						for (const auto& side : SIDES) {
+#ifdef R_SIMD_ENABLED
+							static const __m128 zero = { 0, 0, 0, 0 };
+							static const __m128 mChunk = { CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE };
+
+							__m128 add = _mm_hadd_ps(reinterpret_cast<const __m128&>(pos), reinterpret_cast<const __m128&>(side));
+							float *vec = reinterpret_cast<float *>(&add);
+
+							__m128 greater = _mm_cmpge_ps(add, mChunk);
+							__m128 lesser = _mm_cmplt_ps(add, zero);
+							__m128 combine = _mm_or_ps(greater, lesser);
+							__m128i iCombine = reinterpret_cast<__m128i>(combine);
+							
+							if (_mm_testz_si128(iCombine, iCombine)) {
+								continue;
+							}
+							
+							auto neighbor = chunk.getBlock(vec[0], vec[1], vec[2]);
+
+#else
+							glm::ivec3 vec = pos + side; 
+
+							if (vec.x >= CHUNK_SIZE ||
+								vec.y >= CHUNK_SIZE ||
+								vec.z >= CHUNK_SIZE) {
+								continue;
+							}
+							if (vec.x < 0 ||
+								vec.y < 0 ||
+								vec.z < 0) {
+								continue;
+							}
+
+							auto neighbor = chunk.getBlock(vec.x, vec.y, vec.z);
+#endif
+							
+							if (neighbor == sh::AIR_BLOCK) { 
+								// TODO: other non-solid blocks?
+								data.reserve(data.size() + 8); // 4 verts, 4 texture coords
+							}
+						}
 					}
 				}
 			}
