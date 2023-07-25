da8c4a7ccbb996ea18527be3e1ad028c23282e5d
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2023-07-25 02:15:22 -0700

    Continue on mods

--- mods/luajit/loader.c
+++ mods/luajit/loader.c
@@ -1,3 +1,4 @@
 
+#include <modificate/interface.h>
 
 
--- shared/include/shared/mod.h
+++ shared/include/shared/mod.h
@@ -5,6 +5,9 @@
 #include <stdint.h>
 
 typedef struct {
+    char *id;
+    char *root_path;
+
     // mod permissions
     uint8_t sys_enabled     : 1; // access to the system (bytecode, ffi, jit, fs)
     uint8_t gui_enabled     : 1; // access to creating gui and hud
@@ -12,6 +15,7 @@ typedef struct {
     uint8_t data_enabled    : 1; // access to saving any amount of data to disk
 } mod_t;
 
-void mod_load_();
+int mod_queue(char *root_path);
+mod_t *mod_load(char *root_path);
 
 #endif // SHARED_MOD_H_
